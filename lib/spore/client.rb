98f1e3f7cfcfb6ee8b84b2c007a1dccae16c6a20
Shayan Guha <shayan@coliloquy.com>
2015-05-20 18:27:38 -0700

    Refactor connection settings

--- lib/spore/client.rb
+++ lib/spore/client.rb
@@ -1,4 +1,5 @@
 require 'faraday'
+require 'faraday_middleware'
 # require 'spore/error'
 require 'spore/version'
 require 'spore/client/users'
@@ -10,18 +11,18 @@ module Spore
   class Client
     include Spore::Client::Users
 
-    attr_reader :username
+    attr_reader :email
+    attr_reader :name
+    attr_reader :key
     attr_reader :current_user
 
     attr_accessor :api_endpoint
     attr_accessor :user_agent
     attr_accessor :middleware
 
-    def initialize(username = nil, password = nil)
-      @username = username
-      @password = password
-
-      sign_in(username, password) unless username.nil? || password.nil?
+    def initialize(email = nil, key = nil)
+      @email = email
+      @key = key
     end
 
     def api_endpoint
@@ -29,7 +30,7 @@ module Spore
     end
 
     def user_agent
-      @user_agent ||= "Spore. #{Spore::Version.to_s}"
+      @user_agent ||= "Spore #{Spore::Version.to_s}"
     end
 
     def middleware
@@ -44,46 +45,35 @@ module Spore
     private
 
     def get(path, params = nil)
-      request(:get, path, params, connection)
+      request(:get, path, params)
     end
 
     def post(path, params = nil)
-      request(:post, path, params, connection)
+      request(:post, path, params)
     end
 
-    def put(path, params = nil)
-      request(:put, path, params, connection)
+    def patch(path, params = nil)
+      request(:patch, path, params)
     end
 
-    def delete_path(path, params = nil)
-      request(:delete, path, params, connection)
+    def delete(path, params = nil)
+      request(:delete, path, params)
     end
 
-    def request(method, path, parameters = {}, request_connection)
-      if signed_in?
-        request = authenticated_request_configuration(method, path, parameters)
-        request_connection.send(method.to_sym, path, parameters, &request).env
-      else
-        request_connection.send(method.to_sym, path, parameters).env
-      end
+    def request(method, path, parameters = {})
+      connection.send(method.to_sym, path, parameters).env
     rescue Faraday::Error::ClientError
       raise Spore::RequestError
     end
 
-    def authenticated_request_configuration(method, path, parameters)
-      fail Spore::NotAuthenticated unless signed_in?
-
-      proc do |request|
-      end
-    end
-
     def connection
-      @connection ||= 
-        Faraday.new(api_endpoint) do |conn|
-          conn.request :url_encoded
-          conn.response :json, :content_type => /\bjson$/
-          conn.adapter  Faraday.default_adapter
-        end
+      Faraday.new(api_endpoint) do |conn|
+        conn.basic_auth(email || name, key) if signed_in?
+        conn.request :url_encoded
+        conn.response :json, :content_type => /\bjson$/
+        conn.adapter  Faraday.default_adapter
+        conn.headers[:user_agent] = user_agent
+      end
     end
 
   end
--- lib/spore/client/users.rb
+++ lib/spore/client/users.rb
@@ -1,19 +1,24 @@
 module Spore
   class Client
-
     module Users
-
       def signup(email, password)
-        response = post '/users', { email: email, password: password }
-        body = response[:body]
-
-        data = body[:json][:user]
+        response = post "/users", { email: email, password: password }
+        @verified = response.body["user"]["verified"]
+        login(email, password)
       end
 
       def login(email, password)
+        response = post "/users/#{email}/keys", { password: password }
+        @email = email
+        @key = response.body["key"]
       end
 
       def verify(token)
+        response = patch "/users/#{@email}", { token: token }
+      end
+
+      def signed_in?
+        !email.nil? && !key.nil?
       end
     end
   end
