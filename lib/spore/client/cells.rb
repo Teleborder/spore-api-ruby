db3d3e3340fdb9712914af6525eafcff04cfdad2
Shayan Guha <shayan@coliloquy.com>
2015-05-21 18:18:31 -0700

    Make name consisten

--- lib/spore/client/cells.rb
+++ lib/spore/client/cells.rb
@@ -1,7 +1,7 @@
 module Spore
   class Client
     module Cells
-      def cell_create(app_id, environment, key, value, cell_id = nil)
+      def create_cell(app_id, environment, key, value, cell_id = nil)
         cell_id = SecureRandom.uuid if cell_id.nil?
         path = "/apps/#{app_id}/envs/#{environment}/cells"
         response = post path, { id: cell_id, key: key, value: value }
