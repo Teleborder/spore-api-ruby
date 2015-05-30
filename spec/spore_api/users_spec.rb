5bf4b3e90df2f85ff1122806530473279d9e975e
Shayan Guha <shayan@teleborder.com>
2015-05-29 17:53:01 -0700

    Add successful login spec

--- spec/spore_api/users_spec.rb
+++ spec/spore_api/users_spec.rb
@@ -6,4 +6,11 @@ describe Spore::Client::Users, :vcr do
     expect { client.login "samsymons", "hunter2" }.to raise_error RuntimeError
     expect(client.signed_in?).to be false
   end
+
+  it "should login with valid credentials" do
+    client = Spore::Client.new
+    client.login "example@spore.sh", "fakepassword"
+    expect(client.signed_in?).to be true
+  end
+
 end
